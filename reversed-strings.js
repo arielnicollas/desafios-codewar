abe3758b98d40f5d24be63266fa71d5bac935a47
Ariel Nicollas de Paula Costa <ariel.costa@valecard.com.br>
2022-12-26 17:22:06 -0300

    fix:new challenge

---
+++ reversed-strings.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+
+// Example:
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//My solution:
+
+function solution(str){
+    const reverseString = str.split('').reverse().join("")
+    return reverseString
+  }
